6b6e2a5f263f70f33014d1e5228e0c87590ee6ab
Thomas Neep (Advanced Research Computing) <t.j.neep@bham.ac.uk>
2026-03-16 13:15:28 +0000

    Add function for converting longitude.
    From 0->360 to -180->180

--- src/donalddl_pwrd/earthdatahub/__init__.py
+++ src/donalddl_pwrd/earthdatahub/__init__.py
@@ -3,6 +3,16 @@ from pathlib import Path
 from zarr.storage import LocalStore
 from zarr.experimental.cache_store import CacheStore
 
+def convert_longitude(ds):
+    """Given an xarray dataset, converts longitude from 0->360 to -180->180.
+
+    Usage:
+
+    Given a dataset `ds` use as
+    `ds = ds.assign_coords(longitude=convert_longitude)`
+    """
+    return ((ds.longitude + 180) % 360) - 180
+
 class LocalCache:
 
     def __init__(self, path=None):
