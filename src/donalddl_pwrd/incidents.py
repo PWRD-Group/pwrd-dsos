b24ae92bca2478709571957f6847879f38921267
Thomas Neep (Advanced Research Computing) <t.j.neep@bham.ac.uk>
2026-03-17 17:08:46 +0000

    Move resilience method into accessor

--- src/donalddl_pwrd/incidents.py
+++ src/donalddl_pwrd/incidents.py
@@ -27,7 +27,7 @@ class PwrdFaultsAccessor:
             # Drop duplicate reference numbers so that we only have the main incident (not "sub-incidents")
             .drop_duplicates(reference)
             # Perform a spatial join with areas so each fault is associated to an area
-            # how="right" keeps the geometries from the areas 
+            # how="right" keeps the geometries from the areas
             # and includes any areas that don't contain incidents
             .sjoin(areas, predicate="within", how="right")
             # Make a new column in the dataframe that is the hour the fault occured
@@ -43,13 +43,13 @@ class PwrdFaultsAccessor:
             .rename("faults")
             # Convert to an xarray.DataArray
             .to_xarray()
-            # Reindex so that we have an entry for every hour included in the weather dataset 
+            # Reindex so that we have an entry for every hour included in the weather dataset
             .reindex(time=all_times)
             # Rename hour as valid time for consistency with weather data
             .rename(time="valid_time")
-            # Fill any NA values with 0 
+            # Fill any NA values with 0
             # (if they are NA then it means no incidents occured in that GSP in that hour)
-            .fillna(0)            
+            .fillna(0)
         )
 
         return (
@@ -62,4 +62,30 @@ class PwrdFaultsAccessor:
             .xvec.set_geom_indexes("geometry", crs=areas.crs)
         )
 
-                  
+
+
+    def resilience(self, start, end, customers):
+        """Create a dataframe of outage and restoration customer numbers
+        from a dataframe of incidents.
+        """
+        # We group by the start and end times (which will do a sort by
+        # default) and then we sum up (eventually cumulatively) how
+        # many customers have been restored.  This is under the
+        # assumption that customers are affected at the start of fault
+        # and restored at the end of it.
+
+        # sum then cumsum because a direct cumsum on a groupby does a
+        # cumulative sum per group
+        outage = self._df.groupby(start)[customers].sum().cumsum()
+        restoration = self._df.groupby(end)[customers].sum().cumsum()
+
+        # ffill is used to front fill NaN values that are present in
+        # only one of start or end times, ensuring that there is a
+        # value for every entry.  Once front filling is complete NaN
+        # values are then set to 0 which sets the first entries of the
+        # restoration column when no customers have been restored.
+        return pd.DataFrame(
+            {"outage": outage, "restoration": restoration}
+        ).ffill().fillna(0).assign(
+            resilience=lambda df: df.restoration - df.outage,
+        )
