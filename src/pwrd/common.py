88de21ecd7cd0864ea1314075695d101e349c0dd
Thomas Neep (Advanced Research Computing) <t.j.neep@bham.ac.uk>
2026-04-02 14:33:39 +0100

    Clean up comments

--- src/pwrd/common.py
+++ src/pwrd/common.py
@@ -10,19 +10,24 @@ if TYPE_CHECKING:
 
 
 def _xarray_to_xvec(xarr: xr.DataArray, areas: gpd.GeoDataFrame) -> xr.DataArray:
-    """Convert an xarray to a xvec compatible format using a GeoDataFrame."""
+    """Convert an xarray to a xvec compatible format using a GeoDataFrame.
+
+    Returns
+    -------
+    An xarray.DataArray with a geometry dimension that matches a
+    coordinate which is the index of the input `areas` GeoDataFrame.
+    """
 
     name = areas.index.name
-    # This should be the geometries in the same order as the index of
-    # xarr
+    # Need to convert Geometry array to a numpy object array. The .loc
+    # call hopefully ensures that the xarray is aligned properly with
+    # the geometry. This should mean the geometries are in the same
+    # order as the index of xarr.
     geometries = np.array(areas.loc[xarr[name]].geometry)
 
     return (
         xarr
-        # Assign a new coordinate. Note need to convert Geometry
-        # array to a numpy object array The .loc call hopefully
-        # ensures that the xarray is aligned properly with the
-        # geometry
+        # Assign a new coordinate.
         .assign_coords(geometry=(name, geometries))
         .swap_dims({name: "geometry"})
         .xvec.set_geom_indexes("geometry", crs=areas.crs)
