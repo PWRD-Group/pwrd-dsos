8273334ea6c7a74d83ecf85b3f2c2b3b724a2e3f
Thomas Neep (Advanced Research Computing) <t.j.neep@bham.ac.uk>
2026-04-13 17:04:17 +0100

    Update powerline notebook to use the new accessor

--- notebooks/power_lines.py
+++ notebooks/power_lines.py
@@ -1,10 +1,11 @@
 import marimo
 
-__generated_with = "0.20.4"
+__generated_with = "0.22.4"
 app = marimo.App(width="medium")
 
 with app.setup:
     import marimo as mo
+    import matplotlib.pyplot as plt
     import pandas as pd
     import geopandas as gpd
     from pwrd.huwise import UKPNClient
@@ -23,7 +24,7 @@ def _():
     2. Line data (this notebook)
     3. Area data (e.g. primary areas used in this and other notebooks)
 
-    For now I'm (usually) getting data using the "export" functionality on the UKPN website. Need to decide if it is better to use the API.
+    To aggregate powerline data into areas, we need both the powerline data and the area data.
     """)
     return
 
@@ -33,6 +34,14 @@ def _():
     ukpn_primary_areas = gpd.read_parquet(ukpn_client["ukpn_primary_postcode_area"].file("parquet")).set_geometry("geo_shape")
     power_lines_132kv = gpd.read_parquet(ukpn_client["ukpn-132kv-overhead-lines"].file("parquet")).set_geometry("geo_shape")
     return power_lines_132kv, ukpn_primary_areas
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    We can plot both of these to see where these powerlines are
+    """)
+    return
 
 
 @app.cell
@@ -45,27 +54,27 @@ def _(power_lines_132kv, ukpn_primary_areas):
 @app.cell(hide_code=True)
 def _():
     mo.md(r"""
-    Ok, cool, so we can see the 132kV overhead lines. The next question I suppose is how we can aggregate these by area
+    We can see the 132kV overhead lines. The next question is how we can aggregate these by area. To do this we can use the `line_length_in_areas` method that can be accessed via the `pwrd` accessor. This operates on the line data (the powerlines) and takes areas as an argument. The areas must have a named index. The accessor method also needs an explicit CRS, since not every CRS can be used to compute a meaningful length. The accessor method returns an `xarray` that is in the right format to be used with `xvec` for plotting.
     """)
     return
 
 
 @app.cell
 def _(power_lines_132kv, ukpn_primary_areas):
-    lines_in_areas = gpd.overlay(power_lines_132kv, ukpn_primary_areas, how="intersection")
-    # Make sure we are in a sensible CRS to compute length EPSG:27700 is the OS projection for GB
-    lines_in_areas["length"] = lines_in_areas.to_crs("EPSG:27700").length / 1000.  # km
-
-    ax2 = ukpn_primary_areas.plot(fc="none", lw=0.1)
-    ukpn_primary_areas.join(lines_in_areas.groupby("primary")["length"].sum().reindex(ukpn_primary_areas.primary), on="primary").plot("length", ax=ax2, legend=True, legend_kwds={"label": "Powerline length [km]"})
-    power_lines_132kv.plot(ax=ax2, color="orange", lw=0.5)
+    _, axes = (
+        power_lines_132kv
+            .pwrd.line_length_in_areas(ukpn_primary_areas.set_index("primary"), crs="EPSG:27700")
+            .rename("Powerline length [km]")
+            .xvec.plot(figsize=(5, 5), cmap="BuPu", ec="k", lw=0.1)
+    )
+    power_lines_132kv.plot(ax=axes, color="red", lw=0.5)
     return
 
 
 @app.cell(hide_code=True)
 def _():
     mo.md(r"""
-    This looks pretty good. We can see that only primary areas that contain powerlines seem to have a colour, which suggests it might be working somewhat as we expect. There isn't an immediate correlation between powerline length and number of faults.
+    This looks pretty good. We can see that primary areas that contain powerlines have a darker colour.
     """)
     return
 
@@ -110,23 +119,20 @@ def _():
 
 @app.cell
 def _(all_overhead_lines, ukpn_primary_areas):
-    all_lines_in_areas = gpd.overlay(all_overhead_lines, ukpn_primary_areas, how="intersection")
-    # Make sure we are in a sensible CRS to compute length EPSG:27700 is the OS projection for GB
-    all_lines_in_areas["length"] = all_lines_in_areas.to_crs("EPSG:27700").length / 1000.  # km
-
-    ax4 = ukpn_primary_areas.plot(fc="none", lw=0.1)
-    ukpn_primary_areas.join(
-        all_lines_in_areas.groupby("primary")["length"].sum().reindex(ukpn_primary_areas.primary),
-        on="primary"
-    ).plot("length", ax=ax4, legend=True, legend_kwds={"label": "Powerline length [km]"})
-    all_overhead_lines.plot(ax=ax4, color="orange", lw=0.1)
+    _, axes_all = (
+        all_overhead_lines
+            .pwrd.line_length_in_areas(ukpn_primary_areas.set_index("primary"), crs="EPSG:27700")
+            .rename("Powerline length [km]")
+            .xvec.plot(figsize=(5, 5), cmap="BuPu", ec="k", lw=0.1)
+    )
+    all_overhead_lines.plot(ax=axes_all, color="red", lw=0.1)
     return
 
 
 @app.cell(hide_code=True)
 def _():
     mo.md(r"""
-    Ok, so it takes longer to run (about 45 seconds on my machine) but it seems to work!
+    It takes longer to run (about 45 seconds on my machine) but gives us the powerline length for all overhead powerlines in the UKPN area.
     """)
     return
 
