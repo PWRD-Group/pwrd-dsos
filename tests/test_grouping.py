b063b708683419f802eadb1c40abaa961c20a5dd
Thomas Neep (Advanced Research Computing) <t.j.neep@bham.ac.uk>
2026-04-29 14:13:25 +0100

    Add tests for groupby on line_length_in_areas

--- tests/test_grouping.py
+++ tests/test_grouping.py
@@ -4,7 +4,7 @@ import geopandas as gpd
 import pandas as pd
 
 import shapely
-from shapely.geometry import Polygon
+from shapely.geometry import Polygon, LineString
 
 
 import pwrd  # noqa: F401
@@ -21,6 +21,26 @@ def gb_extents():
     boxes = [Polygon([(-7, 50), (2, 50), (2, 59), (-7, 59)])]
     index = pd.Index(["GB"], name="area")
     areas = gpd.GeoDataFrame(geometry=boxes, index=index, crs=WGS_CRS)
+    return areas.to_crs(BNG_CRS)
+
+
+@pytest.fixture
+def split_uk_extents():
+    # Rough extents of UK split up into different regions. Very rough, don't complain.
+    boxes = [
+        Polygon([(-8, 55), (1, 55), (1, 60), (-8, 60)]),  # Scotland
+        Polygon([(-8, 54), (-5, 54), (-5, 55), (-8, 55)]),  # NI
+        Polygon([(-5, 51), (-3, 51), (-3, 54), (-5, 54)]),  # Wales
+        Polygon([(-4, 54), (0, 54), (0, 55), (-4, 55)]),  # England 1 (north)
+        Polygon([(-3, 50), (2, 50), (2, 54), (-3, 54)]),  # England 2 (Most)
+        Polygon(
+            [(-6, 50), (-3, 50), (-3, 51), (-6, 51)]
+        ),  # England 3 (Cornwall & Devon)
+    ]
+
+    index = pd.Index(range(len(boxes)), name="area")
+    areas = gpd.GeoDataFrame(geometry=boxes, index=index, crs=WGS_CRS)
+    areas["country"] = ["Scotland", "NI", "Wales", "England", "England", "England"]
     return areas.to_crs(BNG_CRS)
 
 
@@ -44,6 +64,28 @@ def test_line_length_in_areas(gb_extents, unnamed_areas):
     # Areas must have a named index to work, test that is the case
     with pytest.raises(ValueError):
         lines.pwrd.line_length_in_areas(unnamed_areas, crs=BNG_CRS)
+
+
+@pytest.mark.parametrize(
+    "line,crossings",
+    [
+        (LineString([(-2, 51), (-2, 59)]), 2),
+        (LineString([(-8, 54.5), (1, 54.5)]), 2),
+        (LineString([(-4, 50), (-4, 60)]), 3),
+        (LineString([(-9, 57), (1, 57)]), 1),
+    ],
+)
+def test_line_length_in_areas_groupby(split_uk_extents, line, crossings):
+    # One line in the area, one outside. Only the length of the line
+    # inside the area will actually be calculated.
+    lines = gpd.GeoDataFrame(geometry=[line], crs=WGS_CRS)
+    lines = lines.to_crs(BNG_CRS)
+
+    lengths = lines.pwrd.line_length_in_areas(
+        split_uk_extents, crs=BNG_CRS, groupby="country"
+    )
+    assert set(lengths.country.values) == {"England", "Wales", "Scotland", "NI"}
+    assert (lengths > 0).sum().item() == crossings
 
 
 def test_points_in_areas(gb_extents, unnamed_areas):
