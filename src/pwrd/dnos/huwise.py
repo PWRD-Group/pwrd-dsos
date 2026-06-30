4eff124c935fdf1147f47ac3831f5039b36fd089
Thomas Neep (Advanced Research Computing) <t.j.neep@bham.ac.uk>
2026-06-30 11:22:46 +0100

    Tell ruff to ignore checking docstrings of methods marked override

--- src/pwrd/dnos/huwise.py
+++ src/pwrd/dnos/huwise.py
@@ -2,6 +2,7 @@
 
 import logging
 from functools import cached_property
+from typing import override
 
 
 from pwrd.dnos import base
@@ -21,6 +22,7 @@ class Resource(base.Resource):
         """The record count."""
         return int(self.info["metas"]["default"]["records_count"])
 
+    @override
     def export_paths(self) -> dict[str, str]:
         r = self.client.client.get(f"/{self.name}/exports")
         r.raise_for_status()
--- src/pwrd/dnos/nged.py
+++ src/pwrd/dnos/nged.py
@@ -3,6 +3,7 @@
 import logging
 from functools import cached_property
 from pathlib import Path
+from typing import override
 
 from pwrd.dnos import base
 
@@ -19,6 +20,7 @@ class Resource(base.Resource):
         # stem works a bit better for compatibility
         return str(Path(self.info["resource"]["url"]).stem)
 
+    @override
     def export_paths(self) -> dict[str, str]:
         info = self.info["resource"]
         return {info["format"].lower(): info["url"]}
