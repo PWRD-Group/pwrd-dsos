729297660255dbe8d0bb21aa4c4b3dd4b7bceb4a
Thomas Neep (Advanced Research Computing) <t.j.neep@bham.ac.uk>
2026-07-06 11:30:34 +0100

    Add logic to check if we have a standard or classic API key
    Both are allowed, but if we have both we'll use the 'standard' key

--- src/pwrd/earthdatahub/era5.py
+++ src/pwrd/earthdatahub/era5.py
@@ -1,8 +1,51 @@
 """ERA5 data stores from <https://earthdatahub.destine.eu>."""
 
+from netrc import netrc
+import warnings
+
 from zarr.storage import FsspecStore
 
-BASE_URL = "https://data.earthdatahub.destine.eu/era5"
+
+def _find_api_endpoint() -> str:
+    """Find EarthDataHub API endpoint to use.
+
+    EarthDataHub recently (Summer 2026) updated their API endpoint,
+    and different tokens are used for each one. To allow for a smooth
+    transition from one to the other we query the .netrc file, and
+    will use the newer endpoint if available and the older if not.
+
+    Returns
+    -------
+    str
+        The url of the endpoint to use
+    """
+    hosts = netrc().hosts
+
+    endpoint = "api.earthdatahub.destine.eu"
+    if endpoint in hosts:
+        # "Standard API key exists - good!"
+        return endpoint
+
+    # Check if we have a Classic API key - fine if we do"
+    endpoint = "data.earthdatahub.destine.eu"
+    if endpoint in hosts:
+        msg = (
+            "EarthDataHub has introduced a new API endpoint that "
+            "requires a new API token. Please see "
+            "https://earthdatahub.destine.eu/account-settings "
+            "for more information. pwrd will use your Classic API "
+            "key but please obtaining a new key.
+        )
+        warnings.warn(msg)
+        return endpoint
+
+    # If we can't find anything then raise an exception
+    msg = "Could not find earthdatahub API key in .netrc file"
+    raise KeyError(msg)
+
+
+BASE_URL = f"https://{_find_api_endpoint()}/era5"
+
 ERA5_STORAGE_OPS = {"client_kwargs": {"trust_env": True}}
 
 land_hourly_store = FsspecStore.from_url(
